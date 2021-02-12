2443bb928b52afeab8e2451c00df49ea52b820a8
Bobby Roe <bobbyroe@gmail.com>
2021-02-12 11:46:30 -0800

    gulp serve is serving!

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,31 @@
-const GulpClient = require('gulp');
-const gulp = require('gulp');
-const nunjucks = require('gulp-nunjucks');
-const sass = require('gulp-sass');
+const GulpClient = require("gulp");
+const gulp = require("gulp");
+const nunjucks = require("gulp-nunjucks");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const browserSync = require("browser-sync").create();
 
-// function initWatch () {
-//     gulp.watch(['./*.njk'], (cb) => {
-//         gulp.src('./index.njk')
-//             .pipe(nunjucks.compile())
-//             .pipe(gulp.dest('./zzz'));
-//         // cb();
-//     });
-// }
+function compileSass() {
+  return gulp
+    .src("assets/styles/**/*.scss")
+    .pipe(sass())
+    .pipe(rename({ extname: ".also.processed.css" })) // TODO – confirm that css is correct
+    .pipe(gulp.dest("assets/styles"));
+}
 
-gulp.task('sass', () => {
-    return gulp.src('assets/styles/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('assets/styles'))
-});
+function complieNunjucks () {
+  return gulp.src("./*.njk").pipe(nunjucks.compile()).pipe(gulp.dest("./"));
+}
+
+module.exports = {
+  serve: gulp.series(compileSass, complieNunjucks, () => {
+    function refresh(done) {
+      browserSync.reload();
+      done();
+    }
+    browserSync.init({
+      server: "./",
+    });
+    gulp.watch(["./**/*.njk", "assets/styles/**/*.scss"], gulp.series(compileSass, complieNunjucks, refresh));
+  }),
+};
