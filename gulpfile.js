b45b61310562d7b0e0adbf9100fe629d7f36136c
Bobby Roe <bobbyroe@gmail.com>
2021-02-11 16:02:30 -0800

    gulp-sass & gulp-nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
-function defaultTask(cb) {
-    cb();
-}
+const GulpClient = require('gulp');
+const gulp = require('gulp');
+const nunjucks = require('gulp-nunjucks');
+const sass = require('gulp-sass');
 
-exports.default = defaultTask;
+// function initWatch () {
+//     gulp.watch(['./*.njk'], (cb) => {
+//         gulp.src('./index.njk')
+//             .pipe(nunjucks.compile())
+//             .pipe(gulp.dest('./zzz'));
+//         // cb();
+//     });
+// }
+
+gulp.task('sass', () => {
+    return gulp.src('assets/styles/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('assets/styles'))
+});
